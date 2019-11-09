1b6fa865074fc4b13b286fe5ee6dc7fd87c1a62f
widad <boujaafar58@gmail.com>
2019-11-09 16:36:50 +0100

    add home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+import Cover from "./assets/background-image.jpg";
+import "./assets/css/index.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="home-container">
+          <img className="cover" src={Cover} alt=""></img>
+          <div className="primary-text">A social network for all your</div>
+          <div className="secondary-text">FOOD DESIRES</div>
+          <div className="home-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            sollicitudin est ex, quis egestas nisi tempus et.
+          </div>
+          <button
+            className="sign-up-button"
+            style={{ position: "relative", bottom: "15rem", left: "10%" }}
+          >
+            Get Started
+          </button>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Home;
