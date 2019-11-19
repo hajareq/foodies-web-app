1c8f7a5bcd638d7832ee75ca7295ae5acd12f431
EL QOTNI Hajar <haajar.eq@gmail.com>
2019-11-19 10:21:14 +0100

    add profile component

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
+import Profile from "./Profile";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route exact path="/SignIn" component={SignIn} />
             <Route exact path="/SignUp" component={SignUp} />
+            <Route exact path="/Profile" component={Profile} />
           </Switch>
         </Router>
       </Fragment>
